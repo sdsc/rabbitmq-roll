85438f14f97195a9def8f0db09e4ee30d3502ca8
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-11-30 17:42:48 -0800

    Foundation packages and bootstrap

---
+++ src/foundation-python-lockfile/version.mk
@@ -0,0 +1,6 @@
+NAME	= foundation-python-lockfile
+RELEASE = 0
+VERSION = 0.12.2
+PACKAGES = lockfile-0.12.2
+RPM.REQUIRES = foundation-python-pbr
+RPM.FILES = "$(PY.ROCKS)/*"
---
+++ src/foundation-python-pbr/version.mk
@@ -0,0 +1,5 @@
+NAME	= foundation-python-pbr
+RELEASE = 0
+VERSION = 3.1.1
+PACKAGES = pbr-3.1.1
+RPM.FILES = "$(PY.ROCKS)/*\\n\/opt/rocks/bin/*"
